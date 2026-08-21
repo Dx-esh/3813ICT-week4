47eab115e5a8fcb39bdc64c24e7e50b5a8bcab6f
Dxesh <daxtonlind@outlook.com>
2026-08-21 10:09:12 +1000

    Typos and minor fixes

--- src/app/components/login-component/login.component.ts
+++ src/app/components/login-component/login.component.ts
@@ -6,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClient } from '@angular/common/http';
 
-import { Customer } from '../../models/customer';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-login.component',
@@ -24,21 +24,44 @@ import { Customer } from '../../models/customer';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+
   message = signal('');
+  messageType: string = '';
 
   router = inject(Router);
   httpclient = inject(HttpClient);
 
   onSubmit() {
-    console.log('Email', this.email);
-    console.log('Password', this.password);
-    //this.email = this.email + ' Some other text';
-
-    this.httpclient.post<Customer>('http://localhost:3000/api/login', {email: this.email, password: this.password}).subscribe(
-      (response) => {
+    this.httpclient.post<User>('http://localhost:3000/api/auth', { email: this.email, password: this.password }).subscribe({
+      next: (response) => {
         console.log(response);
-        this.message.set(response.message);
+
+        if (response.valid) {
+          localStorage.setItem('user', JSON.stringify({
+            username: response.username,
+            birthDate: response.birthDate,
+            age: response.age,
+            email: response.email,
+            valid: response.valid
+          }));
+
+          this.messageType = 'success';
+          this.message.set('Login successful');
+
+          this.router.navigate(['/profile']);
+
+        } else {
+          this.messageType = 'fail';
+          this.message.set('Invalid email or password');
+
+        }
+      },
+      error: (error) => {
+        console.error(error);
+
+        this.messageType = 'fail';
+        this.message.set('Unable to connect to server');
       }
-    );
+    });
   }
 }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    _id!: string;
+    username!: string;
+    birthDate!: Date;
+    age!: number;
+    email!: string;
+    password!: string;
+    valid!: boolean;
+    message!: string;
+}
